0c2ac563df3714c326be93edc1d5c2e346d2c8b7
Joey Hamilton <jhamilton744@gmail.com>
2020-06-23 15:11:16 -0500

    Now handling weeks that span months. (I think...)

--- fetch-gtj.py
+++ fetch-gtj.py
@@ -17,14 +17,6 @@ url = 'https://bbn1.bbnradio.org/english/home/all-programs/gateway-to-joy-elisab
 page = requests.get(bbn_url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-notes = '''
-Currently this program doesn't take into account the first or last
-week of the month (when monday_date might be in a different month
-or any days)
-'''
-
-print(notes)
-
 #
 # get all of the titles from the 'calendar' at the bottom of the page
 # This creates an array of filenames indexed by day_of_month (i.e. 1 to 31)
@@ -45,27 +37,26 @@ for day in range(1, 31):
 
 today = datetime.date.today()
 monday_date = today - datetime.timedelta(days=today.weekday())
-monday_day_of_month = monday_date.day
 
-weekdays = ('monday','tuesday','wednesday','thursday','friday')
-day_of_month = {}
 i = 0
-for day in weekdays:
-    day_of_month[day] = monday_day_of_month + i
-    print(day,monday_day_of_month+i)
-    i = i + 1
-
-dom = monday_day_of_month
 for day in ('monday','tuesday','wednesday','thursday','friday'):
-    day_player_page = requests.get(url+day)
-    day_player_soup = BeautifulSoup(day_player_page.content,'html.parser')
-    su_audio_div = day_player_soup.find('div',class_='su-audio')
-    if su_audio_div:
-        m4a_url = su_audio_div['data-audio']
-        print( "wget","-nc -O",filenames[dom],m4a_url)
-        r = requests.get(m4a_url, allow_redirects=True)
-        open(filenames[dom],'wb').write(r.content)
+    day_date = monday_date + datetime.timedelta(days=i)
+    if day_date.month == today.month:
+        dom = day_date.day
+        day_player_page = requests.get(url+day)
+        day_player_soup = BeautifulSoup(day_player_page.content,'html.parser')
+        su_audio_div = day_player_soup.find('div',class_='su-audio')
+        day_str = day_date.year+day_date.month+day_date.day
+        if su_audio_div:
+            m4a_url = su_audio_div['data-audio']
+            fn = day_date.strftime("%Y%m%d")+"-"+filenames[dom]
+            print( "wget","-nc -O",fn,m4a_url)
+            r = requests.get(m4a_url, allow_redirects=True)
+            open(fn,'wb').write(r.content)
+        else:
+            print ('No m4a found for "' + day + '"')
+
     else:
-        print ('No m4a found for "' + day + '"')
-    dom = dom + 1
+        print("Skipping",day,".  It occurs in a different month.")
+    i = i + 1
 
